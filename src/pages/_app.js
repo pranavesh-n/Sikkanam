589e4e9b4e85d1624e4e9b65487c3dadd2e87149
Pranavesh N <pranaveshnandakumar@gmail.com>
2026-06-11 14:26:30 +0530

    Remove service worker update functionality

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,10 @@
 import WhatsNewModal from '@/components/WhatsNewModal';
-import { useServiceWorker } from '@/hooks/useServiceWorker';
 
 export default function App({ Component, pageProps }) {
-  const { applyUpdate } = useServiceWorker();
-
   return (
     <>
       <Component {...pageProps} />
-      <WhatsNewModal onRefresh={applyUpdate} />
+      <WhatsNewModal />        
     </>
   );
 }
